e76d79661ae2d730ec92bcd93d0662b54ce45e7a
Ahmed Ismail <76683876+El-Mradny@users.noreply.github.com>
2021-09-23 11:04:03 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,122 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+const allSections = document.querySelectorAll("section>div>h2");
+let sectionIds = [];
+let navList = document.querySelector("#navbar__list");
+let newNode = document.createDocumentFragment();
+let scrollFlag = null;
+let navBar =  document.querySelector('.navbar__menu');
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+let format = (item) =>{
+    return item.replace(' ', '').toLowerCase()
+}
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+const initiate = () => {
+    allSections.forEach((item)=> {
+        sectionIds.push(format(item.innerHTML));
+        let newItemNav = document.createElement("li");
+        newItemNav.innerHTML = "<a class='menu__link'  href= '#"+format(item.innerHTML)+ "'>"+item.innerHTML+"</a>";
+        newNode.appendChild(newItemNav);
+        newItemNav.setAttribute("id", format(item.innerHTML)+"-nav");
+    });
+    navList.appendChild(newNode)
+    collapseSections();
+}
+
+
+// Add class 'active' to section when near top of viewport
+// Scroll to anchor ID using scrollTO event
+// check which element are shown in screen
+const checkActive = () =>{
+    navBar.style.display="block";
+    if (scrollFlag !== null) {
+        clearTimeout(scrollFlag)
+        for (let i = 0 ; i < sectionIds.length; i++){
+            let sectionItem = document.getElementById(sectionIds[i]);
+            sectionItem.setAttribute("class", '');
+            sectionItem.addEventListener()
+            showInViewport(sectionItem , sectionIds[i] );
+        }
+    }
+    scrollFlag = setTimeout(() => {
+        navBar.style.display="none";
+    }, 500);
+}
+
+
+// Modify active section and nav bar after checking Viewport.
+let showInViewport = (tagElement, navElementClass) =>{
+    const rect = tagElement.getBoundingClientRect();
+    const navElement = document.querySelector(`#${navElementClass}-nav`);
+    if  (
+        rect.top >= -14 &&
+        rect.top <= 800
+    ){
+        tagElement.setAttribute("class", "active");
+        tagElement.style.scrollBehavior= 'smooth';
+        navElement.style.backgroundColor = "#333";
+    }else {
+        navElement.style.backgroundColor = "";
+    }
+    
+}
+
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
+
+
+
+initiate();
+window.addEventListener('scroll', checkActive);
+
+
+
+
+
+
+
+
